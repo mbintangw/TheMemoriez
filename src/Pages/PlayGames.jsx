300c4e35a58ec197cdfb52bd90e2145992bf7482
mbintangw <mbintangw@gmail.com>
2024-05-07 16:52:48 +0700

    makes cards flip and makes the same card a match

--- src/Pages/PlayGames.jsx
+++ src/Pages/PlayGames.jsx
@@ -3,25 +3,65 @@ import { Link } from 'react-router-dom'
 import Cards from '../components/Cards'
 
 const cardImages = [
-  {"image": "img/vintage.jpg"},
-  {"image": "img/crimson.jpg"},
-  {"image": "img/daisy.jpg"},
-  {"image": "img/lily.jpg"},
-  {"image": "img/pink-swamp.jpg"},
-  {"image": "img/saffron.jpg"},
+  {"image": "img/vintage.jpg", matched : false},
+  {"image": "img/crimson.jpg", matched : false},
+  {"image": "img/daisy.jpg", matched : false},
+  {"image": "img/lily.jpg", matched : false},
+  {"image": "img/pink-swamp.jpg", matched : false},
+  {"image": "img/saffron.jpg", matched : false},
 ]
 
 const PlayGames = () => {
   const [cards, setCards] = useState([])
   const [turns, setTurns] = useState(0)
+  const [pickFirst, setpickFirst] = useState(null)
+  const [pickSecond, setpickSecond] = useState(null)
+  const [selectedCard, setselectedCard] = useState (null)
 
   const shuffleCards = () => {
     const shuffleCards = [...cardImages, ...cardImages]
     .sort(() => Math.random() - 0.5)
     .map((card) => ({ ...card, id: Math.random() }))
-  
+    
+    setpickFirst(null)
+    setpickSecond(null)
     setCards(shuffleCards)
     setTurns(0)
+  }
+
+  const handleChoice = (card) => {
+    pickFirst ? setpickSecond(card) : setpickFirst(card)
+    setselectedCard(card.id)
+  }
+
+  useEffect(() => {
+    if (pickFirst && pickSecond) {
+      if (pickFirst.image === pickSecond.image) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if (card.image === pickFirst.image) {
+              return {...card, matched: true}
+            } else {
+              return card
+            }
+          })
+        })
+        console.log('You found a match')
+        resetTurn()
+      } else {
+        console.log('Sorry, try again')
+        setTimeout(() => {
+          setselectedCard(null);
+          resetTurn();
+        }, 500)
+      }
+    }
+  }, [pickFirst, pickSecond])
+
+  const resetTurn = () => {
+    setpickFirst(null)
+    setpickSecond(null)
+    setTurns(prevTurns => prevTurns + 1)
   }
 
   useEffect(() => {
@@ -32,12 +72,17 @@ const PlayGames = () => {
     shuffleCards();
   }
 
-  console.log(cards,turns)
   return (
       <div className='flex flex-col items-center justify-center gap-5'>
+        <h1>Turns : {turns}</h1>
         <div className='card-grid'>
           {cards.map ((card) => (
-            <Cards key={card.id} card={card} />
+            <Cards 
+              key={card.id} 
+              card={card} 
+              handleChoice={handleChoice}
+              selectedCard={selectedCard}
+              />
           ))}
         </div>
         <Link to={`/`} className='button-primary text-center mt-10 !w-1/2'>Home</Link>
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import backcard from '/img/back-card.jpg'
+import ReactCardFlip from 'react-card-flip'
 
-export default function Cards({card}) {
+export default function Cards({card, handleChoice, flipped, selectedCard}) {
+  
+  const handleClick = () => {
+    handleChoice(card)
+  }
+  
   return (
     <div key={card.id} className='card'>
+      <ReactCardFlip isFlipped={selectedCard !== card.id && !card.matched}>
+
+      <img src={card.image} alt='front card' className=''/>
       <div>
-        <img src={card.image} alt='front card'/>
-        <img src={backcard} alt='back card' />
+        <img 
+          src={backcard} 
+          alt='back card' 
+          onClick= {() => handleClick(card.id)}
+          />
       </div>
-  </div>
+
+    </ReactCardFlip>
+    </div>
+    
   )
 }
+{/* <div key={card.id} className='card'>
+<div className={flipped ? 'flipped' : ''}>
+  <img src={card.image} alt='front card' className='absolute'/>
+  <img 
+    src={backcard} 
+    alt='back card' 
+    onClick={handleClick}
+    />
+</div>
+</div> */}
